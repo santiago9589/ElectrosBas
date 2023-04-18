a3e5ed05bf5919d37bd915e1ddc5c45fc5086536
santiago9589 <santiagodejesus587@gmail.com>
2023-04-17 23:03:33 -0300

    initial configuration

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import HomePage from "./pages/home/home.page"
+
+import React from "react"
+
+
+
+function App() {
+
+  return (
+      <HomePage />
+  )
+}
+
+export default App
---
+++ src/pages/home/home.page.tsx
@@ -0,0 +1,14 @@
+
+
+
+const HomePage = () => {
+
+  return (
+    <>
+     <h1 className="underline text-red-600">vvsdfsdf</h1>
+    </>
+  )
+}
+
+export default HomePage
+
