3f25e5e6be00600619aec19ad1d09fda646d25e6
santiago9589 <santiagodejesus587@gmail.com>
2023-05-07 20:17:12 -0300

    set bg-color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='text-4xl capitalize col-span-6 grid place-items-center'>
+    <footer className='bg-gradient-to-r from-slate-200 via-white to-slate-200 text-4xl capitalize col-span-6 grid place-items-center'>
         Todos los derechos reservados
     </footer>
   )
